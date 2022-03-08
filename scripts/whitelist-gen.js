07968e4f1672d9142e197281dea330571dfc913b
Kenneth Toxcon <kennethtoxro@gmail.com>
2022-03-08 15:40:22 -0600

    feat(scripts): adding suggestions to script

--- scripts/whitelist-gen.js
+++ scripts/whitelist-gen.js
@@ -1,8 +1,27 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { existsSync } = require("fs");
+const { resolve, join } = require("path");
+const { writeFile, readFile, mkdir } = require("fs/promises");
 
-function getPayeesFromFile(dataSourceFilePath) {
+async function getWhiteListFinalPath(fileName) {
   try {
-    const payeesData = readFileSync(dataSourceFilePath);
+    const finalDirPath = resolve(join(__dirname, `/../artifacts/data/`));
+    const finalDirPathExists = existsSync(finalDirPath);
+
+    if (!finalDirPathExists) {
+      await mkdir(finalDirPath);
+    }
+
+    const finalPath = join(finalDirPath, fileName);
+
+    return finalPath;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getPayeesFromFile(dataSourceFilePath) {
+  try {
+    const payeesData = await readFile(dataSourceFilePath);
     const payees = JSON.parse(payeesData.toString("UTF-8"));
 
     return payees;
@@ -20,23 +39,25 @@ function calculateShares(payees, shareValue) {
   return shares;
 }
 
-function generateWhiteList() {
-  const args = process.argv.slice(2);
-
-  if (!args.length) {
-    throw new Error("At least one parameter is expected: data source file");
-  }
-
-  const dataSourceFilePath = args[0];
-  const endFilePath = args[1] || "./whitelist.json";
-  const shareValue = Number(args[2]) || 150;
-
-  const payees = getPayeesFromFile(dataSourceFilePath);
-  const shares = calculateShares(payees, shareValue);
-  const data = JSON.stringify({ payees, shares });
-
+async function generateWhiteList() {
   try {
-    writeFileSync(endFilePath, data);
+    const args = process.argv.slice(2);
+
+    if (!args.length) {
+      throw new Error("At least one parameter is expected: data source file");
+    }
+
+    const dataSourceFilePath = args[0];
+    const fileName = args[1] || "whitelist.json";
+    const shareValue = Number(args[2]) || 150;
+
+    const payees = await getPayeesFromFile(dataSourceFilePath);
+    const shares = calculateShares(payees, shareValue);
+
+    const endFilePath = await getWhiteListFinalPath(fileName);
+    const data = JSON.stringify({ payees, shares });
+
+    await writeFile(endFilePath, data);
   } catch (error) {
     throw error;
   }
